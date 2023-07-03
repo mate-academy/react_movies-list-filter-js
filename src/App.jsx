9b01da49d5a74539dfbc1b9ada3787b0ba53cad4
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-07-03 13:53:30 +0300

    movies filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function prepareMovies(movies, searchFilter) {
+  const moviesList = movies.filter((movie) => {
+    const commonMovieTitle = movie.title.toLowerCase();
+    const commonMovieDescription = movie.description.toLowerCase();
+    const commonFilter = searchFilter.toLowerCase().trim();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+    return commonMovieTitle.includes(commonFilter)
+      || commonMovieDescription.includes(commonFilter);
+  });
+
+  return moviesList;
+}
+
+export const App = () => {
+  const [searchFilter, setSearchFilter] = useState('');
+  const visibleMovies = prepareMovies(moviesFromServer, searchFilter);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={searchFilter}
+                onChange={(event) => {
+                  setSearchFilter(event.target.value);
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
