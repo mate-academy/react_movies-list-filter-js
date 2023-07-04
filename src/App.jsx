61822efd24f834476d51c6c6ec4d9161d2e0c412
Alyona Sarapina <alyona.sarapina@gmail.com>
2023-07-04 17:04:51 +0100

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+const getFilteredMovies = (array, query) => {
+  let copyMovies = [...array];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (query) {
+    copyMovies = copyMovies.filter(
+      movie => movie.title.toLowerCase().includes(query.toLowerCase().trim())
+      || movie.description.toLowerCase().includes(query.toLowerCase().trim()));
+  }
+
+  return copyMovies;
+};
+
+export const App = () => {
+  const [queryField, setQueryFiled] = useState('');
+
+  const filteredMovies = getFilteredMovies(moviesFromServer, queryField);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label
+              htmlFor="search-query"
+              className="label"
+            >
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                value={queryField}
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={e => setQueryFiled(e.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={filteredMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
