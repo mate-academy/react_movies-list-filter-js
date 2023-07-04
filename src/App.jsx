ad17a4701799c6a1fc06c98f48c4f91340b0a6e8
Anatolii Lykhohodenko <anatollykh10@gmail.com>
2023-07-04 22:09:33 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [condition, setCondition] = useState('');
+  const visibleMovies = getVisibleMovies(moviesFromServer, condition);
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  function getVisibleMovies(movies, query) {
+    let changedMovies = [...movies];
+
+    if (query) {
+      changedMovies = changedMovies.filter(movie =>
+        // eslint-disable-next-line implicit-arrow-linebreak
+        movie.title.toLowerCase().includes(query.toLowerCase().trim())
+        // eslint-disable-next-line max-len
+        || movie.description.toLowerCase().includes(query.toLowerCase().trim()));
+    }
+
+    return changedMovies;
+  }
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={event => setCondition(event.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
