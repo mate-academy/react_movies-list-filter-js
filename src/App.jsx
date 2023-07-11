867b953a9260d3192ebfe7b9baeb528c38e29bef
Misha Siv <0Sivcov0@gmail.com>
2023-07-11 20:16:56 +0300

    add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getMovie(movies, query) {
+  const searchQuery = query.trim().toLowerCase();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (searchQuery.length > 0) {
+    return movies.filter(el => el.title.toLowerCase().includes(searchQuery)
+      || el.description.toLowerCase().includes(searchQuery));
+  }
+
+  return movies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getMovie(moviesFromServer, query);
+  const searchQuery = event => setQuery(event.target.value);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-qery" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={searchQuery}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar" data-cy="Sidebar">
+        Sidebar will be here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
