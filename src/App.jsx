2f9b2b48c82d0f7c970cefd7ef52cd30d72d949d
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-07-19 11:26:06 +0300

    new version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
@@ -6,43 +6,43 @@ import moviesFromServer from './api/movies.json';
 
 export const App = () => {
   const [query, setQuery] = useState('');
+  const [filteredMovies, setFilteredMovies] = useState([]);
 
   const filterMovies = () => {
     const searchQuery = query.toLowerCase().trim();
 
-    return moviesFromServer.filter((movie) => {
+    const movieSearch = moviesFromServer.filter((movie) => {
       const { title, description } = movie;
 
-      return (title.toLowerCase().includes(searchQuery)
-      || description.toLowerCase().includes(searchQuery));
+      return (
+        title.toLowerCase().includes(searchQuery)
+        || description.toLowerCase().includes(searchQuery)
+      );
     });
+
+    setFilteredMovies(movieSearch);
   };
 
-  const inputChange = (event) => {
-    setQuery(event.target.value);
+  useEffect(() => {
     filterMovies();
-  };
-
-  const filteredMovies = filterMovies();
+  }, [query]);
 
   return (
     <div className="page">
       <div className="page-content">
         <div className="box">
           <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
-              Search movie
-            </label>
-
             <div className="control">
-              <input
-                type="text"
-                id="search-query"
-                className="input"
-                placeholder="Type search word"
-                onChange={inputChange}
-              />
+              <label htmlFor="search-query" className="label">
+                Search movie
+                <input
+                  type="text"
+                  id="search-query"
+                  className="input"
+                  placeholder="Type search word"
+                  onChange={event => (setQuery(event.target.value))}
+                />
+              </label>
             </div>
           </div>
         </div>
