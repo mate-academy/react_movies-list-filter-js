ec12554c115c7bade485ed262ef0d87aa1c88fcc
Vladislav Tsymbalist <vladislavtsimba1999@gmail.com>
2023-07-22 18:50:04 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function filterMovie(goods, { query }) {
+  let copyGoods = [...goods];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (query) {
+    const lowercaseQuery = query.toLowerCase();
+
+    copyGoods = copyGoods.filter(movie => (
+      movie.title.toLowerCase().includes(lowercaseQuery)
+      || movie.description.toLowerCase().includes(lowercaseQuery)
+    ));
+  }
+
+  return copyGoods;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const filteredMovies = filterMovie(moviesFromServer, { query });
+
+  const filterBy = (newQuery) => {
+    setQuery(newQuery);
+  };
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={(event) => {
+                  filterBy(event.target.value);
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={filteredMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
