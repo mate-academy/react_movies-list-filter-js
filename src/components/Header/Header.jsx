8a0ccb44acf49c67c080905f7515498118dfe13f
Antonina Klishch <tonya91t@gmail.com>
2023-08-07 13:24:06 +0300

    Solution

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+
+export const Header = ({
+  query,
+  filterBy,
+}) => (
+  <div className="box">
+    <div className="field">
+      <label htmlFor="search-query" className="label">
+        Search movie
+      </label>
+
+      <div className="control">
+        <input
+          type="text"
+          id="search-query"
+          className="input"
+          placeholder="Type search word"
+          value={query}
+          onChange={(e) => {
+            filterBy(e.target.value);
+          }}
+        />
+      </div>
+    </div>
+  </div>
+);
