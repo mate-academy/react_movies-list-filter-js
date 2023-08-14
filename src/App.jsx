4eb123a76a06bcbe15d6f97d6e9c72ee13df385e
Hanna-Balabukha <anna.aniskovets1992@ukr.net>
2023-08-14 18:11:27 +0300

    'done'

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,51 @@
+import { useState } from 'react';
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
+
 import moviesFromServer from './api/movies.json';
+import { MoviesList } from './components/MoviesList';
+import { Field } from './components/Field/Field';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getPreparedMovies(movies, { query }) {
+  const preparedMovies = [...movies];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
+  if (query) {
+    const part = query.trim().toLowerCase();
+
+    return preparedMovies.filter((movie) => {
+      const movieTitle = movie.title.toLowerCase();
+      const movieDescription = movie.description.toLowerCase();
+
+      return (movieTitle.includes(part) || movieDescription.includes(part));
+    });
+  }
+
+  return preparedMovies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPreparedMovies(moviesFromServer, { query });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <Field
+            query={query}
+            filterBy={(newQuery) => {
+              setQuery(newQuery);
+            }}
+          />
         </div>
+
+        <MoviesList
+          movies={visibleMovies}
+        />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
---
+++ src/components/Field/Field.jsx
@@ -0,0 +1,20 @@
+export const Field = ({ filterBy, query }) => (
+  <div className="field">
+    <label htmlFor="search-query" className="label">
+      Search movie
+    </label>
+
+    <div className="control">
+      <input
+        type="text"
+        id="search-query"
+        className="input"
+        placeholder="Type search word"
+        value={query}
+        onChange={(event) => {
+          filterBy(event.target.value);
+        }}
+      />
+    </div>
+  </div>
+);
