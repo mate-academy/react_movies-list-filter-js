63504248651889b3ce84ce51f6e92fd71d63a7c5
Mykhailo Shevchuk <mishashevchuk@gmail.com>
2023-08-14 23:42:29 +0300

    Search field was implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,62 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getVisibleMovies(movies, { searchInput }) {
+  let visibleMovies = [...movies];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (searchInput) {
+    visibleMovies = visibleMovies.filter((movie) => {
+      const input = searchInput.trim().toLowerCase();
+      const movieTitle = movie.title.toLowerCase();
+      const movieDescription = movie.description.toLowerCase();
+      const isMatch = movieTitle.includes(input)
+        || movieDescription.includes(input);
+
+      return isMatch;
+    });
+  }
+
+  return visibleMovies;
+}
+
+export const App = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const visibleMovies = getVisibleMovies(moviesFromServer, { searchInput });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={(event) => {
+                  setSearchInput(event.currentTarget.value);
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList
+          movies={visibleMovies}
+        />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
