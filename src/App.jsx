c30428935356cdfbd94ced4cc736785d4587ea52
Eugene Ivchenko <ivchenkoevhen@gmail.com>
2023-08-16 17:41:43 +0300

    finished version

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
 import './App.scss';
+import { useState } from 'react';
+
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { Control } from './components/Control/control';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
+  const filteredMovies = moviesFromServer.filter(
+    movie => movie.title.toLowerCase().includes(searchQuery.toLowerCase())
+    || movie.description.toLowerCase().includes(searchQuery.toLowerCase()),
+  );
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <Control
+              searchQuery={searchQuery}
+              setSearchQuery={(newQuery) => {
+                setSearchQuery(newQuery);
+              }}
             />
           </div>
         </div>
+
+        <MoviesList movies={filteredMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here   s
-    </div>
-  </div>
-);
+  );
+};
