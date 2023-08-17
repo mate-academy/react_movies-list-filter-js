e3a9d351f5131a57090f7ee942897a0d872be2cb
Daniel Hladkyi <gladkiy1518@gmail.com>
2023-08-17 10:28:54 +0300

    Solution

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,22 @@
+export const SearchBar = ({ setQuery }) => (
+  <div className="box">
+    <div className="field">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor="search-query" className="label">
+        Search movie
+      </label>
+
+      <div className="control">
+        <input
+          type="text"
+          id="search-query"
+          className="input"
+          placeholder="Type search word"
+          onChange={(event) => {
+            setQuery(event.target.value);
+          }}
+        />
+      </div>
+    </div>
+  </div>
+);
