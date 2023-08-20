12483ff3d6cb47ce0641514e69c74a3581b268f7
Dmytro Fefelov <reseneweb@gmail.com>
2023-08-20 14:28:45 +0300

    added Control.jsx

---
+++ src/components/Control/Control.jsx
@@ -0,0 +1,18 @@
+export const Control = ({
+  filterBy,
+}) => (
+  <div
+    className="control"
+
+  >
+    <input
+      type="text"
+      id="search-query"
+      className="input"
+      placeholder="Type search word"
+      onChange={(event) => {
+        filterBy(event.target.value);
+      }}
+    />
+  </div>
+);
