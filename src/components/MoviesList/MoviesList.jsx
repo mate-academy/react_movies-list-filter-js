5a56a76e8637083ecca048ed47c16b066b8c400f
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-25 20:15:42 +0300

    initial commit

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,23 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  let preparedMovies = [...movies];
+
+  if (query) {
+    preparedMovies = movies
+      .filter(({ title, description }) => title.toLowerCase().includes(query)
+      || description.toLowerCase().includes(query));
+  }
+
+  return (
+    <div className="movies">
+      {preparedMovies.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          movie={movie}
+        />
+      ))}
+    </div>
+  );
+};
