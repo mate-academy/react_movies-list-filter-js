ff241576e8ad268bc9c1e6ee9660f5d97d2ab51e
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-08-27 18:28:36 +0300

    add task solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,29 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+function filterMoviesByQuery(movies, query) {
+  const lowerCaseQuery = query
+    .toLowerCase()
+    .trim();
+
+  const filteredMovies = movies.filter(({ title, description }) => {
+    const searchedMovie = title.toLowerCase().includes(lowerCaseQuery)
+      || description.toLowerCase().includes(lowerCaseQuery);
+
+    return searchedMovie;
+  });
+
+  return filteredMovies;
+}
+
+export const MoviesList = ({ movies, query }) => {
+  const visibleMovies = filterMoviesByQuery(movies, query);
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
