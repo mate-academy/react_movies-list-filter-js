356ea1aac245d5a0304d5832de1bca7d18ffd71f
Rostyslav Kravchuk <mr.shadov.05@gmail.com>
2023-08-28 21:29:38 +0300

    add filter to the MovieList

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,22 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
+export const MoviesList = ({ movies, query }) => (
   <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
+    {movies
+      .filter(movie => includesQuery(query, movie))
+      .map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
   </div>
 );
+
+function includesQuery(query, movie) {
+  const lowerQuery = query.toLowerCase().trim();
+  const lowerTitle = movie.title.toLowerCase();
+  const lowerDescription = movie.description.toLowerCase();
+
+  return (
+    lowerTitle.includes(lowerQuery) || lowerDescription.includes(lowerQuery)
+  );
+}
