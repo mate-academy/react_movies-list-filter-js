8f4fe538bda834f1eb05a7e59be6c33f4ae7b0c0
neovendano <kolesnyk.vik@gmail.com>
2023-08-30 00:03:34 +0300

    add movies list filter by current searchbar query

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function isContainingQuery(movie, query) {
+  const lowercaseQuery = query.toLowerCase();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  return movie.title.toLowerCase().includes(lowercaseQuery)
+    || movie.description.toLowerCase().includes(lowercaseQuery);
+}
+
+function getMoviesToRender(movies, query) {
+  return movies.filter(movie => isContainingQuery(movie, query));
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getMoviesToRender(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={
+                  event => setQuery(event.target.value.trim().toLowerCase())
+                }
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
