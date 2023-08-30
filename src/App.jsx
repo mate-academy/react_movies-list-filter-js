a0d85b64faf64cf4e4664b0a0621d1dc43367a4f
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-08-30 11:07:09 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
 import './App.scss';
+import { useEffect, useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+const getFilteredMovies = (movies, query) => movies.filter((movie) => {
+  const formattedTitle = movie.title.toLowerCase();
+  const formattedDescription = movie.description.toLowerCase();
+  const formattedQuery = query.toLowerCase().trim();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  return formattedTitle.includes(formattedQuery)
+    || formattedDescription.includes(formattedQuery);
+});
+
+export const App = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState([...moviesFromServer]);
+
+  useEffect(() => {
+    switch (searchValue) {
+      case '':
+        setVisibleMovies(moviesFromServer);
+        break;
+      default:
+        setVisibleMovies(getFilteredMovies(moviesFromServer, searchValue));
+    }
+  }, [searchValue]);
+
+  const searchChangeHandler = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  return (
+
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                value={searchValue}
+                onChange={searchChangeHandler}
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
