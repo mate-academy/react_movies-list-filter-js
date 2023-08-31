0da226ed987e5c93915b554fdd23d6cf4499b53a
Sergey <serg9205@gmail.com>
2023-08-31 10:13:29 +0300

    improve filter function

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
 function getFilteredMovies(movies, query) {
-  const normalizeQuery = query.toLowerCase().trim();
+  const normalizedQuery = query.toLowerCase().trim();
 
   return movies.filter(movie => (
-    movie.title.toLowerCase().includes(normalizeQuery)
-    || movie.description.toLowerCase().includes(normalizeQuery)
+    movie.title.includes(normalizedQuery)
+    || movie.description.includes(normalizedQuery)
   ));
 }
 
