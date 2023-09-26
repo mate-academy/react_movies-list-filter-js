b7cc56f5841e9f13bc92553dbe01ce31c02cf6bb
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-09-26 21:36:05 +0200

    Changed code

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,11 @@ import './App.scss';
 import moviesFromServer from './api/movies.json';
 import { Page } from './components/Page/Page';
 
-// function findQuery(string, query) {
-//   return string.toLowerCase().trim().includes(query);
-// }
-
 function getPreparedMovies(movies, { query }) {
   let preparedMovies = [...movies];
 
   const preparedQuery = query.trim().toLowerCase();
 
-  // if (query) {
-  //   preparedMovies
-  //   = preparedMovies.filter(movie => findQuery(movie.title, query)
-  //   || findQuery(movie.description, query));
-  // }
   if (preparedQuery) {
     preparedMovies
     = preparedMovies.filter(movie => movie.title.toLowerCase()
