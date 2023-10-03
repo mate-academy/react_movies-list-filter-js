f5585fd9204a9ed1aeb670a6ca8c51daaa3f1a17
Artem Trofymovych <navrotskymetra@outlook.com>
2023-10-03 12:13:10 +0200

    filter implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,64 @@
 import './App.scss';
+import { useState } from 'react';
+
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getFilteredFilms(movies, query) {
+  let visibleFilms = [...movies];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (query) {
+    visibleFilms = visibleFilms
+      .filter(film => film.title
+        .toLowerCase()
+        .replace(/\s+/g, '')
+        .includes(query)
+    || film.description
+      .toLowerCase()
+      .replace(/\s+/g, '')
+      .includes(query));
+  }
+
+  return visibleFilms;
+}
+
+export const App = () => {
+  const [filterField, setFilterField] = useState('');
+
+  const visibleFilms = getFilteredFilms(moviesFromServer, filterField);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={(event) => {
+                  setFilterField(event.currentTarget.value
+                    .toLowerCase()
+                    .replace(/\s+/g, ''));
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleFilms} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
