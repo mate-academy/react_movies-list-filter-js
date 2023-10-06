fe8e4242ec7b6365c5d498b4d10ce409d4aaabce
donizer <bogdan.ua45@gmail.com>
2023-10-06 09:58:30 +0300

    search filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
+import './App.scss';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function prepareArray(array, { query }) {
+  let cloneArr = [...array];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (query) {
+    cloneArr = cloneArr.filter(elem => (
+      elem.title.toLowerCase().includes(query)
+    || elem.description.toLowerCase().includes(query)
+    ));
+  }
+
+  return cloneArr;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibeMovies = prepareArray(moviesFromServer, { query });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={e => setQuery(e.target.value.toLowerCase().trim())}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibeMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
