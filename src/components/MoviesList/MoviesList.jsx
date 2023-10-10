3ca4cca072b5a492246ffbfa6920d298b794eac2
Vovchuk <kulivackijvoloda@gmail.com>
2023-10-10 18:32:20 +0300

    add task solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,21 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  const filteredMovies = movies.filter((movie) => {
+    const lowerCaseQuery = query.toLowerCase();
+
+    return (
+      movie.title.toLowerCase().includes(lowerCaseQuery)
+     || movie.description.toLowerCase().includes(lowerCaseQuery)
+    );
+  });
+
+  return (
+    <div className="movies">
+      {filteredMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
