a6d13d331fc356da3e4963b241b7ef51897e910d
BetterSol <94358102+BetterSol@users.noreply.github.com>
2023-10-11 22:09:00 +0300

    solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
+// import React from 'react';
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
+import { useState } from 'react';
 import moviesFromServer from './api/movies.json';
+import { MoviesList } from './components/MoviesList';
+import { SearchBar } from './components/SearchBar/SearchBar';
+// import { set } from 'cypress/types/lodash';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getMovies(movies, { query }) {
+  let filteredMovies = movies;
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+  if (query) {
+    filteredMovies = movies.filter(
+      movie => movie.title.toLowerCase().includes(query.trim().toLowerCase())
+      || movie.description.toLowerCase().includes(query.trim().toLowerCase()),
+    );
+  }
+
+  return filteredMovies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = getMovies(
+    moviesFromServer,
+    { query },
+  );
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <SearchBar
+          filterBy={(newQuery) => {
+            setQuery(newQuery);
+          }}
+        />
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
