93d2a8cdd08f37df267c4113ab7685ead44dbb83
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-10-11 13:00:25 +0200

    movies searching implemented

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,29 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+const filterList = (movies, query) => {
+  let filteredList = [...movies];
+
+  if (query) {
+    const normalizedQuery = query.trim().toLowerCase();
+
+    filteredList = movies.filter(movie => (
+      movie.title.toLowerCase().includes(normalizedQuery)
+        || movie.description.toLowerCase().includes(normalizedQuery)
+    ));
+  }
+
+  return filteredList;
+};
+
+export const MoviesList = ({ movies, query }) => {
+  const filteredMovies = filterList(movies, query);
+
+  return (
+    <div className="movies">
+      {filteredMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
