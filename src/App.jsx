1098c11723c2a79ff61a3f38105fb1eaafabda80
Kateryna Sydorenko <katerynnnna@gmail.com>
2023-10-25 15:37:45 +0300

    task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,59 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+const getVisibleMovies = (movies, { sortBy }) => {
+  let filteredMovies = [...movies];
+  const sort = sortBy.trim().toLowerCase();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (sort) {
+    /* eslint-disable-next-line */
+    filteredMovies = filteredMovies.filter((movie) => {
+      const title = movie.title.toLowerCase();
+      const description = movie.description.toLowerCase();
+
+      if (title.includes(sort) || description.includes(sort)) {
+        return movie;
+      }
+    });
+  }
+
+  return filteredMovies;
+};
+
+export const App = () => {
+  const [sortBy, setSortBy] = useState('');
+  const visibleMovies = getVisibleMovies(moviesFromServer, { sortBy });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={e => setSortBy(e.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
