a4b541621e20025656365a4c4528e682112a86bf
Sakhno Andrii <andreysakhno1986@gmail.com>
2023-11-10 13:33:03 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
-import './App.scss';
+import { useState } from 'react';
+
+import { MovieSearch } from './components/MoviesSearch/MoviesSearch';
 import { MoviesList } from './components/MoviesList';
+
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+import './App.scss';
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+const isContainsSubstring = (string, subString) => (
+  string.toLowerCase()
+    .includes(
+      subString.trim().toLowerCase(),
+    )
+);
+
+const getPreperedGoods = (goods, query) => {
+  let preperedGoods = [...goods];
+
+  if (query) {
+    preperedGoods = preperedGoods.filter(({ title, description }) => (
+      isContainsSubstring(title, query)
+      || isContainsSubstring(description, query)
+    ));
+  }
+
+  return preperedGoods;
+};
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = getPreperedGoods(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <MovieSearch
+          query={query}
+          search={newQuery => setQuery(newQuery)}
+        />
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
