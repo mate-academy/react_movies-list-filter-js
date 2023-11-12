246611e5f09be42ea25b099ecbb33d428cab1c81
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-11-12 16:40:00 +0100

    made filter search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,74 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+// export function filterSearchBar(query) {
+//   let copyOfMovies = [...moviesFromServer];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+//   const lowerCaseQuery = query.toLowerCase().trim();
+//   const lowerCaseTitle = copyOfMovies.title.toLowerCase();
+//   const lowerCaseDescriptions = copyOfMovies.description.toLowerCase();
+
+//   const queryForFilter = lowerCaseDescriptions.includes(lowerCaseQuery)
+//     || lowerCaseTitle.includes(lowerCaseQuery);
+
+//   if (query) {
+//     copyOfMovies = copyOfMovies.filter(item => queryForFilter);
+//   }
+
+//   return copyOfMovies;
+// }
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const filterMovies = (movies) => {
+    const lowerCaseQuery = query.toLowerCase().trim();
+    const lowerCaseTitle = movies.title.toLowerCase();
+    const lowerCaseDescriptions = movies.description.toLowerCase();
+
+    const queryForFilter = lowerCaseDescriptions.includes(lowerCaseQuery)
+      || lowerCaseTitle.includes(lowerCaseQuery);
+
+    return queryForFilter;
+  };
+
+  const visibleMovies = [...moviesFromServer].filter(filterMovies);
+
+  const handleQueryChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={handleQueryChange}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+}
