af813f06932ba9038a1a938b9dcb1e40fb7b0680
Ivan <your@mail.com>
2023-11-14 12:24:48 +0200

    filter movies create!

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [filter, setFilter] = useState('');
+  const visibleMovies = moviesFromServer.filter(movie => (
+    movie.title.toLowerCase()
+      .includes(filter.toLowerCase().trim())) || movie.description.toLowerCase()
+    .includes(filter.toLowerCase().trim()));
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                onChange={(event) => {
+                  setFilter(event.target.value);
+                }}
+                className="input"
+                placeholder="Type search word"
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
