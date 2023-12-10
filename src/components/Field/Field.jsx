d79bd32c9ac127ca9f7bdbe2792a906f4fe046f8
boytrend0108 <boytrend@gmail.com>
2023-12-10 15:02:56 +0200

    complete task

---
+++ src/components/Field/Field.jsx
@@ -0,0 +1,19 @@
+export const Field = ({ query, filterBy }) => (
+  <div className="field">
+    {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+    <label htmlFor="search-query" className="label">
+      Search movie
+    </label>
+
+    <div className="control">
+      <input
+        value={query}
+        type="text"
+        id="search-query"
+        className="input"
+        placeholder="Type search word"
+        onChange={event => filterBy(event.target.value)}
+      />
+    </div>
+  </div>
+);
