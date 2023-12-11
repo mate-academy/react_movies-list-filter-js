521c9f1fd198c5a4c492cbe460119f4efc215bcd
SvitlanaRamanauskas <ramanauskassvs@gmail.com>
2023-12-11 12:42:50 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
+import { useState } from 'react';
+
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { MoviesList } from './components/MoviesList';
+import { Box } from './components/Box';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function includesQuery(input, query) {
+  return input.trim().toLowerCase().includes(query.trim().toLowerCase());
+}
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+function getFilteredMovies(movies, query) {
+  let preparedMovies = [...movies];
+
+  preparedMovies = preparedMovies.filter((movie) => {
+    const titleMatch = includesQuery(movie.title, query);
+    const descriptionMatch = includesQuery(movie.description, query);
+
+    return titleMatch || descriptionMatch;
+  });
+
+  return preparedMovies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = getFilteredMovies(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <Box
+          filterBy={setQuery}
+          query={query}
+        />
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
