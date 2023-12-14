78142aafc061ff717b7ce78cf1f3a6170523016c
ArtyWhit <julwer1k@gmail.com>
2023-12-14 19:01:40 +0200

    finished solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
+import { useState } from 'react';
 import moviesFromServer from './api/movies.json';
+import { Field } from './components/Field';
+import { MoviesList } from './components/MoviesList';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function updateMovies(movies, query) {
+  return movies.filter((movie) => {
+    const titleToLowerCase = movie.title.toLowerCase();
+    const descriptionToLowerCase = movie.description.toLowerCase();
+    const queryTrimSpacesToLowerCase = query.trim().toLowerCase();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
+    return (
+      titleToLowerCase.includes(queryTrimSpacesToLowerCase)
+      || descriptionToLowerCase.includes(queryTrimSpacesToLowerCase)
+    );
+  });
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = updateMovies(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <Field
+            filterBy={setQuery}
+            query={query}
+          />
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
