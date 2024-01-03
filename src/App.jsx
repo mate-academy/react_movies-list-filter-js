cb90624a4fca341e90e8ce3971e125142646c7b4
Evhen Holub <golub0208@gmail.com>
2024-01-03 17:36:46 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { FilterCase } from './components/FilterCase';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getPreparedMovies(movies, { query }) {
+  let preparedMovies = [...movies];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+  if (query) {
+    preparedMovies = preparedMovies.filter(
+      movie => movie.title
+        .toLowerCase()
+        .includes(query.toLowerCase().trim())
+      || movie.description
+        .toLowerCase()
+        .includes(query.toLowerCase().trim()),
+    );
+  }
+
+  return preparedMovies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const visisibleMovies = getPreparedMovies(moviesFromServer, { query });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <FilterCase filterBy={(newQuery) => {
+          setQuery(newQuery);
+        }}
+        />
+
+        <MoviesList movies={visisibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">
+        Sidebar goes here
+      </div>
     </div>
-
-    <div className="sidebar">
-      Sidebar goes here
-    </div>
-  </div>
-);
+  );
+};
---
+++ src/components/FilterCase/FilterCase.jsx
@@ -0,0 +1,22 @@
+export const FilterCase = ({ filterBy }) => (
+  <div className="box">
+    <div className="field">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor="search-query" className="label">
+        Search movie
+      </label>
+
+      <div className="control">
+        <input
+          onChange={(event) => {
+            filterBy(event.target.value);
+          }}
+          type="text"
+          id="search-query"
+          className="input"
+          placeholder="Type search word"
+        />
+      </div>
+    </div>
+  </div>
+);
