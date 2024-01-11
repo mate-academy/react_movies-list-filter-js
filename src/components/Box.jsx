bc333786253e79cf0373c2efd99dbb98be05536a
Victoria Pukalo <pukalovictoria@gmail.com>
2024-01-11 16:57:17 +0100

    Solution

---
+++ src/components/Box.jsx
@@ -0,0 +1,26 @@
+export const Box = ({
+  query,
+  filterBy,
+}) => (
+  <div className="box">
+    <div className="field">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor="search-query" className="label">
+        Search movie
+      </label>
+
+      <div className="control">
+        <input
+          value={query}
+          type="text"
+          id="search-query"
+          className="input"
+          placeholder="Type search word"
+          onChange={(event) => {
+            filterBy(event.target.value);
+          }}
+        />
+      </div>
+    </div>
+  </div>
+);
