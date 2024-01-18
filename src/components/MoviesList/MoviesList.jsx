658dbbbef019f43fe76a08c8e71812c07ee90dbb
Ihor Pavlov <ihorpavlov@gmail.com>
2024-01-18 12:59:46 +0200

    add task solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,19 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  const visibleMovies = movies.filter(movie => (
+    movie.title.toLowerCase().includes(query.toLowerCase().trim())
+    || movie.description.toLowerCase().includes(query.toLowerCase().trim())));
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard
+          key={movie.imdbId}
+          movie={movie}
+        />
+      ))}
+    </div>
+  );
+};
