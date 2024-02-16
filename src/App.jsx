6e3d364d27672dc379db0d5c6f4a138021ba8df7
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-02-16 17:54:00 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,63 @@
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function filterMovies(movies, { query }) {
+  const normalQuery = query.toLowerCase().trim();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (!normalQuery) {
+    return movies;
+  }
+
+  const filteredMovies = movies.filter(movie => {
+    const normaliseTitle = movie.title.toLowerCase();
+    const description = movie.description.toLowerCase();
+
+    return (
+      normaliseTitle.includes(normalQuery) || description.includes(normalQuery)
+    );
+  });
+
+  return filteredMovies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleFilms = filterMovies(moviesFromServer, { query });
+
+  const hendleSearch = event => {
+    setQuery(event.target.value);
+  };
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                value={query}
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={hendleSearch}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleFilms} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
