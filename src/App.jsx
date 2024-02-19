689e4c74794e256cb3f1e46c318cb0b77825ed08
ArsenSkandalenko <arsenskandal@gmail.com>
2024-02-19 20:00:01 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,26 @@
-import { useState } from 'react';
 import './App.scss';
+import { useState } from 'react';
+
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function isTextIcludes(text1, text2, part) {
+  const isFirstTextIncludes = text1
+    .toLowerCase()
+    .includes(part.toLowerCase().trim());
+  const isSecondTextIncludes = text2
+    .toLowerCase()
+    .includes(part.toLowerCase().trim());
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-function getPreparedMovies(movies, query) {
-  let preparedMovies = [...movies];
-
-  if (query) {
-    const lowerQuery = query.toLowerCase().trim();
-
-    preparedMovies = preparedMovies.filter(movie => {
-      const title = movie.title.toLowerCase().includes(lowerQuery);
-      const description = movie.description.toLowerCase().includes(lowerQuery);
-
-      return title || description;
-    });
-  }
-
-  return preparedMovies;
+  return isFirstTextIncludes || isSecondTextIncludes;
 }
 
 export const App = () => {
   const [query, setQuery] = useState('');
-  const visibleMovies = getPreparedMovies(moviesFromServer, query);
+
+  const visibleMovies = moviesFromServer.filter(movie => {
+    return isTextIcludes(movie.description, movie.title, query);
+  });
 
   return (
     <div className="page">
@@ -66,12 +47,7 @@ export const App = () => {
         <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
       <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
   );
 };
