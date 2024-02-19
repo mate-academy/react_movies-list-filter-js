c2f7d4091bd1777e1742b9633021b259c1497f58
ArsenSkandalenko <arsenskandal@gmail.com>
2024-02-19 19:48:45 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
@@ -19,13 +20,58 @@ export const App = () => (
               className="input"
               placeholder="Type search word"
             />
+function getPreparedMovies(movies, query) {
+  let preparedMovies = [...movies];
+
+  if (query) {
+    const lowerQuery = query.toLowerCase().trim();
+
+    preparedMovies = preparedMovies.filter(movie => {
+      const title = movie.title.toLowerCase().includes(lowerQuery);
+      const description = movie.description.toLowerCase().includes(lowerQuery);
+
+      return title || description;
+    });
+  }
+
+  return preparedMovies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPreparedMovies(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={event => setQuery(event.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
       <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
 
     <div className="sidebar">Sidebar goes here</div>
   </div>
 );
+  );
+};
