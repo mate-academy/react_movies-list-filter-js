470260edb3e44a4b1f4f48f326a54c52529d7921
Olha Shvets <lv160888soi@gmail.com>
2024-02-20 12:11:40 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
-import { Searchbar } from './components/Searchbar';
 
 export const App = () => {
   const [query, setQuery] = useState('');
@@ -19,12 +18,24 @@ export const App = () => {
     <div className="page">
       <div className="page-content">
         <div className="box">
-          <Searchbar
-            query={query}
-            onFilterChange={newQuery => {
-              setQuery(newQuery);
-            }}
-          />
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                value={query}
+                onChange={e => {
+                  setQuery(e.target.value);
+                }}
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+              />
+            </div>
+          </div>
         </div>
 
         <MoviesList movies={visibleMovies} />
