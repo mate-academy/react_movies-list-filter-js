81c4f9caad17f40e92e2b5f8c61aa8ad1ebe6178
Yehor-f2006 <151622806+Yehor-f2006@users.noreply.github.com>
2024-02-22 01:04:58 +0200

    solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,21 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  const criteria = query.toLowerCase().trim();
+
+  const visibleMovies = movies.filter(movie => {
+    return (
+      movie.title.toLowerCase().includes(criteria) ||
+      movie.description.toLowerCase().includes(criteria)
+    );
+  });
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
