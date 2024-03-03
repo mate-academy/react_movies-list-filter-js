ab5ad32e04539143924a3906e34a1307b57768d4
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-03-03 11:16:46 +0200

    feat [react]: implement search field for movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+const filterMovies = (movies, query) => {
+  let filteredMovies = [...movies];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (query) {
+    filteredMovies = filteredMovies.filter(
+      movie =>
+        movie.title.toLocaleLowerCase().includes(query) ||
+        movie.description.toLocaleLowerCase().includes(query),
+    );
+  }
+
+  return filteredMovies;
+};
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = filterMovies(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={event => {
+                  setQuery(
+                    event.currentTarget.value.toLocaleLowerCase().trim(),
+                  );
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
