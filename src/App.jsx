4a32e39ea4faddbcc92d9e3d43e1f126dd51e9da
Denis Kotov <dionisiy2904@gmail.com>
2024-03-07 17:56:48 +0200

    React_movies-list-filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,57 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getFilteredList(movies, query) {
+  let prepareMovie = [...movies];
+  const SEARCH_QUARY = query.trim();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (query) {
+    prepareMovie = movies.filter(
+      movie =>
+        movie.title.toLowerCase().includes(SEARCH_QUARY.toLowerCase()) ||
+        movie.description.toLowerCase().includes(SEARCH_QUARY.toLowerCase()),
+    );
+  }
+
+  return prepareMovie;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+
+  const visibleMovies = getFilteredList(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                value={query}
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={event => {
+                  setQuery(event.currentTarget.value);
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,6 +2,7 @@ import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
 export const MoviesList = ({ movies }) => (
+  // export const MoviesList = ({ movies, query, filterList }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
