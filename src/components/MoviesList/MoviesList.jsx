cfb392e65a102e41452cfd2daf0a5107afde4bd8
Denis Kotov <dionisiy2904@gmail.com>
2024-03-07 18:01:44 +0200

    fix movielist

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,7 +2,6 @@ import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
 export const MoviesList = ({ movies }) => (
-  // export const MoviesList = ({ movies, query, filterList }) => (
   <div className="movies">
     {movies.map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
