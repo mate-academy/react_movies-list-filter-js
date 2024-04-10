eb5bb7766d7bf9a134a1a6e836b94a527a157333
Vasyl Chukhrai <wasik1995@gmail.com>
2024-04-10 22:17:29 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
+import { useState } from 'react';
+
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { Box } from './components/Box';
+import { MoviesList } from './components/MoviesList';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getPreperdMovies(movies, { query }) {
+  let preperdMovies = [...movies];
+  const normalizedQuery = query.trim().toLowerCase();
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+  preperdMovies = preperdMovies.filter(
+    ({ title, description }) =>
+      title.toLowerCase().includes(normalizedQuery) ||
+      description.toLowerCase().includes(normalizedQuery),
+  );
+
+  return preperdMovies;
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPreperdMovies(moviesFromServer, { query });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <Box
+          query={query}
+          onQuery={newQuery => {
+            setQuery(newQuery);
+          }}
+        />
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
