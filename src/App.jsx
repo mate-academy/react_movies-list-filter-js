800853d75b9cf209acc448d5d84f63d05d4bc047
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-05-08 12:31:01 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,60 @@
-import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
+
 import moviesFromServer from './api/movies.json';
+import './App.scss';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+const filteredMovies = (initialMovies, { query }) => {
+  let movies = [...initialMovies];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  const prepareQuery = query.trim().toLowerCase();
+
+  if (query) {
+    movies = movies.filter(
+      movie =>
+        movie.title.toLowerCase().indexOf(prepareQuery) !== -1 ||
+        movie.description.toLowerCase().indexOf(prepareQuery) !== -1,
+    );
+  }
+
+  return movies;
+};
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = filteredMovies(moviesFromServer, { query });
+
+  const onChangeQuery = e => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={onChangeQuery}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
