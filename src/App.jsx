24b025ff37034923c677d38e394d5cc9f1f5687b
Tymofii Antoniuk <111820939+timaantonuk@users.noreply.github.com>
2024-06-06 13:24:45 +0100

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,58 @@
 import './App.scss';
+import { useState, useEffect } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [searchInput, setSearchInput] = useState('');
+  const [filteredMovies, setFilteredMovies] = useState([...moviesFromServer]);
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  useEffect(() => {
+    const filterMovies = inputValue => {
+      if (inputValue) {
+        return moviesFromServer.filter(
+          movie =>
+            movie.title
+              .toLowerCase()
+              .includes(inputValue.trim().toLowerCase()) ||
+            movie.description
+              .toLowerCase()
+              .includes(inputValue.trim().toLowerCase()),
+        );
+      }
+
+      return moviesFromServer;
+    };
+
+    setFilteredMovies(filterMovies(searchInput));
+  }, [searchInput]);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={searchInput}
+                onChange={event => setSearchInput(event.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={filteredMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
