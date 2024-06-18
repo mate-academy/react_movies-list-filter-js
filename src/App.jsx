119c4030dcfa7e8b09e6d366febc1d29f2323062
priemssonia <priemssonia@gmail.com>
2024-06-18 23:39:57 +0300

    refersh solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,45 +2,16 @@ import React, { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { MovieInput } from './components/MovieInput';
 
 export const App = () => {
   const [query, setQuery] = useState('');
 
-  const visibleMovies = moviesFromServer.filter(
-    movie =>
-      movie.title.trim().toLowerCase().includes(query.trim().toLowerCase()) ||
-      movie.description
-        .trim()
-        .toLowerCase()
-        .includes(query.trim().toLowerCase()),
-  );
-
   return (
     <div className="page">
       <div className="page-content">
-        <div className="box">
-          <div className="field">
-            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor="search-query" className="label">
-              Search movie
-            </label>
-
-            <div className="control">
-              <input
-                type="text"
-                id="search-query"
-                className="input"
-                placeholder="Type search word"
-                value={query}
-                onChange={event => {
-                  setQuery(event.target.value);
-                }}
-              />
-            </div>
-          </div>
-        </div>
-
-        <MoviesList movies={visibleMovies} />
+        <MovieInput setQuery={setQuery} query={query} />
+        <MoviesList movies={moviesFromServer} query={query} />
       </div>
 
       <div className="sidebar">Sidebar goes here</div>
