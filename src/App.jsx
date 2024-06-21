208c091ba5da635fd12be031f776c8dcfe6c30fb
Ivan Khomenko <gazebo88fire@gmail.com>
2024-06-21 18:15:30 +0300

    Sol

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,63 @@
+import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [visibleMovies, setVisibleMovies] = useState([]);
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  useEffect(() => {
+    setVisibleMovies(filterMovies(moviesFromServer, query));
+  }, [query]);
+
+  const handleSearchInputChange = event => {
+    const newQuery = event.target.value;
+
+    setQuery(newQuery);
+  };
+
+  // eslint-disable-next-line no-shadow
+  const filterMovies = (movies, query) => {
+    if (!query) {
+      return movies;
+    }
+
+    const normalizedQuery = query.toLowerCase().trim();
+
+    return movies.filter(
+      movie =>
+        movie.title.toLowerCase().includes(normalizedQuery) ||
+        movie.description.toLowerCase().includes(normalizedQuery),
+    );
+  };
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={handleSearchInputChange}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
