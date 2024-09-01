6fe15ab0dcc2e9e497f0a38d829fee3986eaff62
Mainaiev <farid.mainaiev@proton.me>
2024-09-01 17:18:59 +0300

    first commit

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,25 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, change, setChange }) => {
+  const getFilter = (moviesArr, changeFilter) => {
+    const trimmedFilter = changeFilter.trim().toLowerCase();
+
+    return moviesArr.filter(
+      movie =>
+        movie.title.toLowerCase().startsWith(trimmedFilter) ||
+        movie.title.toLowerCase().includes(trimmedFilter) ||
+        movie.description.toLowerCase().includes(trimmedFilter),
+    );
+  };
+
+  const visibleMovies = getFilter(movies, change);
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
