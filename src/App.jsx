87058f53417e4d7037303275d4d75878527858a6
Illia Kocherovets <illiakocherovets@gmail.com>
2024-09-11 12:02:02 +0200

    add adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
@@ -6,13 +7,10 @@ import moviesFromServer from './api/movies.json';
 export const App = () => {
   const [query, setQuery] = useState('');
 
-  const handleInputChange = event => {
-    setQuery(event.target.value);
-  };
-
-  const visibleMovies = moviesFromServer.filter(movie =>
-    movie.title.toLowerCase().includes(query.trim().toLowerCase()) ||
-    movie.description.toLowerCase().includes(query.trim().toLowerCase())
+  const visibleMovies = moviesFromServer.filter(
+    movie =>
+      movie.title.toLowerCase().includes(query.trim().toLowerCase()) ||
+      movie.description.toLowerCase().includes(query.trim().toLowerCase()),
   );
 
   return (
@@ -20,23 +18,28 @@ export const App = () => {
       <div className="page-content">
         <div className="box">
           <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor="search-query" className="label">
               Search movie
             </label>
+
             <div className="control">
               <input
                 type="text"
                 id="search-query"
                 className="input"
                 placeholder="Type search word"
-                value={query}
-                onChange={handleInputChange}
+                onChange={event => {
+                  setQuery(event.target.value);
+                }}
               />
             </div>
           </div>
         </div>
+
         <MoviesList movies={visibleMovies} />
       </div>
+
       <div className="sidebar">Sidebar goes here</div>
     </div>
   );
