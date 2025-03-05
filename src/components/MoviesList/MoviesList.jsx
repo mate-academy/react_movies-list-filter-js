291611725bc70d3e7987806345dfedda53913b9f
Andrii Hrushetskyi <hrushetskyi.andrii@gmail.com>
2025-03-05 17:19:33 +0100

    finish 🥳

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,22 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+const cswiSearch = (
+  str,
+  substr, // (Case & Surrounding Whitespace) Insensitive search
+) => str.toUpperCase().includes(substr.trim().toUpperCase());
+
+export const MoviesList = ({ movies, query }) => {
+  const visibleMovies = movies.filter(
+    ({ title, description }) =>
+      cswiSearch(title, query) || cswiSearch(description, query),
+  );
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
