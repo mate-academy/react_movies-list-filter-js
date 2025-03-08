815e38dae02f61e36702562cdbf95df8e703108e
LX-y-ka <alexandravirtualife@gmail.com>
2025-03-09 00:56:36 +0200

    add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
 
-export const App = () => (
+/* eslint-disable-next-line jsx-a11y/label-has-associated-control */
+
+function filterByQuery(query) {
+  const preparedQuery = query.toLowerCase().trim();
+
+  return moviesFromServer.filter(item => {
+    const titleInclude = item.title.toLowerCase().includes(preparedQuery.toLowerCase());
+    const descInclude = item.description.toLowerCase().includes(preparedQuery.toLowerCase());
+
+    return titleInclude || descInclude;
+  });
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleItems = filterByQuery(query);
+
+  return (
   <div className="page">
     <div className="page-content">
       <div className="box">
         <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
           <label htmlFor="search-query" className="label">
             Search movie
           </label>
@@ -18,14 +35,17 @@ export const App = () => (
               id="search-query"
               className="input"
               placeholder="Type search word"
+              onChange={(event) => {
+                setQuery(event.target.value);
+              }}
             />
           </div>
         </div>
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <MoviesList movies={visibleItems} />
     </div>
 
     <div className="sidebar">Sidebar goes here</div>
   </div>
-);
+)};
