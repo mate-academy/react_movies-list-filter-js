4cfafd859201aca31e239d2f9b09109d5d51d024
Uusuff <90629840+Uusuff@users.noreply.github.com>
2025-03-13 10:54:25 +0100

    add task solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,32 @@
+/* eslint-disable function-paren-newline */
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  const movieFiltering = (querys, movie) => {
+    const formattedQuery = querys.trim().toLowerCase();
+    const movieTitle = movie.title.trim().toLowerCase();
+    const movieDescription = movie.description.trim().toLowerCase();
+
+    if (
+      movieTitle.includes(formattedQuery) ||
+      movieDescription.includes(formattedQuery)
+    ) {
+      return true;
+    }
+
+    return false;
+  };
+
+  return (
+    <div className="movies">
+      {movies.map(movie =>
+        movieFiltering(query, movie) ? (
+          <MovieCard key={movie.imdbId} movie={movie} />
+        ) : (
+          ''
+        ),
+      )}
+    </div>
+  );
+};
