3c2e76dff77ba7a4d0dde6e8243ad5aacec76063
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-03-20 21:11:37 +0200

    fix2 task solution

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function filterByQuery(movies, query) {
   const normalizedQuery = query.trim().toLowerCase();
 
   if (normalizedQuery) {
-    movies.filter(
+    return movies.filter(
       movie =>
         movie.title.toLowerCase().includes(normalizedQuery) ||
         movie.description.toLowerCase().includes(normalizedQuery),
