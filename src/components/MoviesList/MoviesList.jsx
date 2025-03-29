9d0cc488f0a9a8e3263c4170711060a274b137c3
Arthur Ogunfuye <arthurity@yahoo.co.uk>
2025-03-29 17:03:20 +0000

    React movie list filter solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,25 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, inputValue }) => {
+  let filteredMovies = movies;
+
+  if (inputValue) {
+    filteredMovies = movies.filter(movie => {
+      const movieTitle = movie.title.toLowerCase();
+      const movieDescription = movie.description.toLowerCase();
+
+      return (
+        movieTitle.includes(inputValue) || movieDescription.includes(inputValue)
+      );
+    });
+  }
+
+  return (
+    <div className="movies">
+      {filteredMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
