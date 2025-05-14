923f4581e0deb0d8fb3bbb6f5c5c098913e777ac
tobias <tobias.harasim@seznam.cz>
2025-05-14 13:28:15 +0200

    lol

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,22 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  const normalizedQuery = query.trim().toLowerCase();
+
+  const filteredMovies = query
+    ? movies.filter(
+        movie => // eslint-disable-line
+          movie.title.toLowerCase().includes(normalizedQuery) ||// eslint-disable-line
+          movie.description.toLowerCase().includes(normalizedQuery),
+      )// eslint-disable-line
+    : movies;
+
+  return (
+    <div className="movies">
+      {filteredMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
