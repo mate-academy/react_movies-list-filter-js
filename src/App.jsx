fb8aae57c409e6a0a82f763cab073abbfa2210fb
Daniel-Huudek <139517145+Daniel-Huudek@users.noreply.github.com>
2025-07-12 13:11:04 -0300

    task done

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
+import React from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import Filter from './components/Filter/Filter';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getPreparedMovies(movies, query) {
+  if (!query) {
+    return movies;
+  }
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+  const lowerCaseQuery = query.toLowerCase().trim();
+
+  return movies.filter(movie => {
+    const title = movie.title.toLowerCase();
+    const description = movie.description.toLowerCase();
+
+    return (
+      title.includes(lowerCaseQuery) || description.includes(lowerCaseQuery)
+    );
+  });
+}
+
+export const App = () => {
+  const [query, setQuery] = React.useState('');
+  const visiblemovies = getPreparedMovies(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <Filter query={query} setQuery={setQuery} />
+
+        <MoviesList movies={visiblemovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,25 @@
+export default function Filter({ query, setQuery }) {
+  return (
+    <div className="box">
+      <div className="field">
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <label htmlFor="search-query" className="label">
+          Search movie
+        </label>
+
+        <div className="control">
+          <input
+            type="text"
+            value={query}
+            id="search-query"
+            className="input"
+            placeholder="Type search word"
+            onChange={e => {
+              setQuery(e.target.value);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
