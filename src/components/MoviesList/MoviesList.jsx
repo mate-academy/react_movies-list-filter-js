3aa42c992de46dacad1733731664957677cebb83
Alex Lesnoy <123098237+bebelekele@users.noreply.github.com>
2025-08-05 21:14:46 +0300

    solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,28 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+function sortMovies(movies, query) {
+  let res = [...movies];
+
+  if (query) {
+    res = res.filter(
+      movie =>
+        movie.title.toLowerCase().includes(query.toLowerCase().trim()) ||
+        movie.description.toLowerCase().includes(query.toLowerCase().trim()),
+    );
+  }
+
+  return res;
+}
+
+export const MoviesList = ({ movies, query }) => {
+  const visibleMovies = sortMovies(movies, query);
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
