26a4c0967d2a3b0acddd7214997fd34ea8153e90
iulii sobol <soboliulii@gmail.com>
2025-08-06 16:33:42 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import './App.scss';
-import { MoviesList } from './components/MoviesList';
+import { useState } from 'react';
+import { MoviesList } from './components/MoviesList/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [query, setQuery] = useState('');
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  let visibleMovies = [...moviesFromServer];
+
+  if (query) {
+    visibleMovies = visibleMovies.filter(
+      movie =>
+        movie.title.toLowerCase().includes(query.toLowerCase()) ||
+        movie.description.toLowerCase().includes(query.toLowerCase()),
+    );
+  }
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={e => setQuery(e.target.value.trim())}
+              />
+            </div>
           </div>
+          <MoviesList movies={visibleMovies} />
         </div>
       </div>
-
-      <MoviesList movies={moviesFromServer} />
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
