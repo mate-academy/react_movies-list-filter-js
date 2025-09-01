5380bf08bc5025b216d1f796cf34e50b68edb679
Nikita Chupahin <nik123chupahin@gmail.com>
2025-09-01 10:40:59 +0300

    added task solution

---
+++ src/components/MovieFilter/MovieFilter.jsx
@@ -0,0 +1,23 @@
+export const MovieFilter = ({ query, onQuery }) => (
+  <div className="box">
+    <div className="field">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor="search-query" className="label">
+        Search movie
+      </label>
+
+      <div className="control">
+        <input
+          value={query}
+          type="text"
+          id="search-query"
+          className="input"
+          placeholder="Type search word"
+          onChange={event => {
+            onQuery(event.target.value);
+          }}
+        />
+      </div>
+    </div>
+  </div>
+);
