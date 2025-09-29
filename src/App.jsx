660a1010c2d7a40ee97c5466e92b6c45d2ecdd71
Volodymyr Kolosov <kolosovvova0@gmail.com>
2025-09-29 16:09:06 +0300

    movies-list-wigth-filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,59 @@
 import './App.scss';
+import { useState } from 'react';
+
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function filterMovies(list, query) {
+  let readyMovies = [...list];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (query) {
+    const readyQery = query.toLowerCase().trim();
+
+    readyMovies = readyMovies.filter(
+      film =>
+        film.title.toLowerCase().includes(readyQery) ||
+        film.description.toLowerCase().includes(readyQery),
+    );
+  }
+
+  return readyMovies;
+}
+
+export const App = () => {
+  const [qery, setQery] = useState('');
+
+  const visibleMovies = filterMovies(moviesFromServer, qery);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={qery}
+                onChange={event => {
+                  setQery(event.target.value);
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
