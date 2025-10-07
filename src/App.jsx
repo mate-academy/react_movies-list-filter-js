56c940fe0eef4cf4c29d3db4257fe87887d0b290
Andrii Linkevych <andrii.linkevych.21@pnu.edu.ua>
2025-10-07 20:11:40 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,44 @@
+import { useState } from 'react';
+
+import moviesFromServer from './api/movies.json';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
-import moviesFromServer from './api/movies.json';
+import { Box } from './components/Box/Box';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [query, setQuery] = useState('');
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+  function getPrepearedMovies(moviesToChange, newQuery) {
+    let prepearedMovies = [...moviesToChange];
+    const normalizeQuery = newQuery.trim();
+
+    if (newQuery) {
+      prepearedMovies = prepearedMovies.filter(
+        movie =>
+          movie.title.toLowerCase().includes(normalizeQuery.toLowerCase()) ||
+          movie.description
+            .toLowerCase()
+            .includes(normalizeQuery.toLowerCase()),
+      );
+    }
+
+    return prepearedMovies;
+  }
+
+  // function filterBy() {
+
+  // }
+  const visibleMovies = getPrepearedMovies(moviesFromServer, query);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <Box query={query} onQuery={setQuery} />
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
