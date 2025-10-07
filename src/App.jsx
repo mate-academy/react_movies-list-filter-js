1f58a245b31ff55816b78ebf9236c48367cee69d
milton <mitnunes.pro.com>
2025-10-07 15:53:38 -0300

    add1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
+import React, { useState } from 'react';  
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
+export const App = () => {
+
+   const [query, setQuery] = useState('');
+
+   const normalizedQuery = query.trim().toLowerCase();
+
+   const visibleMovies = moviesFromServer.filter(
+    movie => {
+      if (!normalizedQuery) {
+        return true; 
+      }
+
+      const title = movie.title.toLowerCase();
+      const description = movie.description.toLowerCase();
+      
+      return (
+        title.includes(normalizedQuery) ||
+        description.includes(normalizedQuery)
+       );
+    });
+
+    return (
+
   <div className="page">
     <div className="page-content">
       <div className="box">
@@ -18,14 +41,17 @@ export const App = () => (
               id="search-query"
               className="input"
               placeholder="Type search word"
+              value={query}
+              onChange={event => setQuery(event.target.value)}
             />
           </div>
         </div>
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <MoviesList movies={visibleMovies} />
     </div>
 
     <div className="sidebar">Sidebar goes here</div>
   </div>
-);
+    );
+};
