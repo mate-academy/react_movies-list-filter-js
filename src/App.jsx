4ee4f5ce8984af3669077695b3c48d9e0f59cf0f
Volodimir <volodimirbryatko@gmail.com>
2025-10-08 21:15:07 +0300

    solution2

--- src/App.jsx
+++ src/App.jsx
@@ -6,41 +6,47 @@ import moviesFromServer from './api/movies.json';
 export const App = () => {
   const [query, setQuery] = useState('');
 
-   let visibleMovies = [...moviesFromServer];
+  let visibleMovies = [...moviesFromServer];
 
-    if (query) {
-      visibleMovies = visibleMovies.filter( movie => movie.title.toLowerCase().includes(query.trim().toLowerCase() ) ||
-    movie.description.toLowerCase().includes(query.trim().toLowerCase()) );
-    }
-  const handleSortByQuery = (query) => {
-    setQuery(query);
+  if (query) {
+    const normalizedQuery = query.trim().toLowerCase();
+    visibleMovies = visibleMovies.filter(
+      movie =>
+        movie.title.toLowerCase().includes(normalizedQuery) ||
+        movie.description.toLowerCase().includes(normalizedQuery),
+    );
   }
-  return (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-              onChange={(event) => handleSortByQuery(event.target.value)}
-            />
+  const handleSortByQuery = query1 => {
+    setQuery(query1);
+  };
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={event => handleSortByQuery(event.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={visibleMovies} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
   );
 };
