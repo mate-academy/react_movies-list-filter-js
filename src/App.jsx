5717cc8667ac526eea1250da910aa323eb37d5d1
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2025-10-12 17:29:28 -0300

    binding the input with the query

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ export const App = () => {
 
             <div className="control">
               <input
+                value={query}
                 type="text"
                 id="search-query"
                 className="input"
