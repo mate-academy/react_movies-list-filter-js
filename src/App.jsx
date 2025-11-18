990200d854ce40674d0a579651d356d4d14779bb
Sagi4A <voitko.katya.1@gmail.com>
2025-11-18 18:21:19 +0200

    perfect with checklist

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,16 @@ import moviesFromServer from './api/movies.json';
 export const App = () => {
   const [query, setQuery] = useState('');
   const normalizedQuery = query.trim().toLowerCase();
-  const visibleMovies = query.trim()
+  const visibleMovies = normalizedQuery
     ? moviesFromServer.filter(movie => {
-      const title = movie.title.toLowerCase();
-      const description = movie.description.toLowerCase();
+        const title = movie.title.toLowerCase();
+        const description = movie.description.toLowerCase();
 
         return (
-        title.includes(normalizedQuery) ||
+          title.includes(normalizedQuery) ||
           description.includes(normalizedQuery)
-      );
-    })
+        );
+      })
     : moviesFromServer;
 
   return (
