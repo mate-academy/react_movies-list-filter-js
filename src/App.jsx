216a0a405610a32f4928c4be7e20732d6109f900
Alex <1inboxna@gmail.com>
2026-01-14 16:33:09 +0200

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,34 @@
+import { useState } from 'react';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { SearchInput } from './components/SearchInput/SearchInput';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+export const App = () => {
+  const [query, setQuery] = useState(''); // стан для інпуту пошуку
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
-        </div>
+  const onChangeValue = e => {
+    setQuery(e.target.value);
+  };
+
+  const normalizedQuery = query.trim().toLowerCase();
+
+  const visibleMovies = moviesFromServer.filter(m => {
+    const title = m.title.toLowerCase();
+    const desc = m.description.toLowerCase();
+
+    return title.includes(normalizedQuery) || desc.includes(normalizedQuery);
+  });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <SearchInput textInput={query} onChange={onChangeValue} />
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
