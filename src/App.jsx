96610b767b718e1d700d61828e12136816a223bf
SageUA <panchyshyn33@gmail.com>
2026-01-24 21:14:06 +0200

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,30 @@
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
 
-export const App = () => (
+
+function getPreparedGoods(goods, { query }) {
+  const copyGoods = [...goods];
+
+  const preparedGoods = copyGoods.filter((good) => {
+  const lowerTitle = good.title.toLowerCase();
+  const lowerDescription = good.description.toLowerCase();
+  const lowerQuery = query.trim().toLowerCase();
+
+  return lowerTitle.includes(lowerQuery) || lowerDescription.includes(lowerQuery);
+  });
+
+  return preparedGoods;
+};
+
+export const App = () => {
+
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPreparedGoods(moviesFromServer, { query });
+
+
+  return (
   <div className="page">
     <div className="page-content">
       <div className="box">
@@ -18,14 +40,16 @@ export const App = () => (
               id="search-query"
               className="input"
               placeholder="Type search word"
+              onChange={(event) => {setQuery(event.target.value)}}
             />
           </div>
         </div>
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <MoviesList movies={visibleMovies} />
     </div>
 
     <div className="sidebar">Sidebar goes here</div>
   </div>
-);
+  )
+};
