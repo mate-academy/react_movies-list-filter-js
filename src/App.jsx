9d7cbdbdf24a4a1fecdb525e924196987756c549
Maksym Tsyrulnyk <maximtsyrulnyk@gmail.com>
2026-02-12 21:15:24 +0200

    done Movie list filter (JS)

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
+import { useState } from 'react';
+import { Field } from './components/Field';
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getPreparedMovie(movies, { query }) {
+  if (!movies || !query) {
+    return movies;
+  }
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
-          </div>
+  const preparedQuery = query.trim().toLowerCase();
+
+  return movies.filter(
+    movie =>
+      movie.title.toLowerCase().includes(preparedQuery) ||
+      (movie.description || '').toLowerCase().includes(preparedQuery),
+  );
+}
+
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const visibleMovies = getPreparedMovie(moviesFromServer, { query });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <Field query={query} setQuery={setQuery} />
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
