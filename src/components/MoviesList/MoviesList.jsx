c3c41f0323bcec7957261012608ff634bcb5538e
ImPEPEGA <nicaly15117@gmail.com>
2026-02-16 14:07:37 -0500

    Solution 1

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,30 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+function filterMovies(movies, { searchQuery }) {
+  let filteredMovies = movies;
+
+  if (searchQuery) {
+    filteredMovies = filteredMovies.filter(
+      movie =>
+        movie.title.toLowerCase().includes(searchQuery.trim().toLowerCase()) ||
+        movie.description
+          .toLowerCase()
+          .includes(searchQuery.trim().toLowerCase()),
+    );
+  }
+
+  return filteredMovies;
+}
+
+export const MoviesList = ({ movies, searchQuery }) => {
+  const visibleMovies = filterMovies(movies, { searchQuery });
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
