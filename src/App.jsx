90777bbba54adcaef710627b88be10227fc5f749
Oleh Skibinskyi <oleg.sk2002@gmail.com>
2026-04-04 16:13:01 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,59 @@
+import { useState } from 'react';
+
 import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+const getVisibleMovies = (movies, { query }) => {
+  let preparedMovies = [...movies];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (query !== '') {
+    preparedMovies = preparedMovies.filter(movie => {
+      const lowerQuery = query.toLowerCase().trim();
+
+      return (
+        movie.title.toLowerCase().includes(lowerQuery) ||
+        movie.description.toLowerCase().includes(lowerQuery)
+      );
+    });
+  }
+
+  return preparedMovies;
+};
+
+export const App = () => {
+  const [filterQuery, setFilterQuery] = useState('');
+  const visibleMovies = getVisibleMovies(moviesFromServer, {
+    query: filterQuery,
+  });
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                value={filterQuery}
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                onChange={event => setFilterQuery(event.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
