87c023672a96bec70867b786ae54f184f7a9ef36
Alternate001 <artem.kushko@publizer.de>
2026-04-28 12:07:24 +0200

    solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,33 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+const Filter = (movies, fragment) => {
+  const result = [];
+
+  movies.forEach(movie => {
+    if (
+      movie.title.toLowerCase().includes(fragment.trim().toLowerCase()) ||
+      movie.description.toLowerCase().includes(fragment.trim().toLowerCase())
+    ) {
+      result.push(movie);
+    }
+  });
+
+  return result;
+};
+
+export const MoviesList = ({ movies, fragment }) => {
+  let visibleMovies = [...movies];
+
+  if (fragment) {
+    visibleMovies = Filter(visibleMovies, fragment);
+  }
+
+  return (
+    <div className="movies">
+      {visibleMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
