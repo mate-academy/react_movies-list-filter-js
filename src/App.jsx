d66100586fc878a61e7100a42fbd99f39229697f
Tausvokse <parhom137@gmail.com>
2026-05-04 17:40:47 +0300

    Added task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,59 @@
 import './App.scss';
+import { useState } from 'react';
+
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+function getVisibleMovies(movies, searchField) {
+  let preparedMovies = [...movies];
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-            />
+  if (searchField) {
+    preparedMovies = preparedMovies.filter(movie => {
+      return (
+        movie.title.toLowerCase().includes(searchField.toLowerCase().trim()) ||
+        movie.description
+          .toLowerCase()
+          .includes(searchField.toLowerCase().trim())
+      );
+    });
+  }
+
+  return preparedMovies;
+}
+
+export const App = () => {
+  const [searchField, setSearchField] = useState('');
+  const visibleMovies = getVisibleMovies(moviesFromServer, searchField);
+
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
+
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={searchField}
+                onChange={event => {
+                  setSearchField(event.currentTarget.value);
+                }}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={moviesFromServer} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
+  );
+};
