2b1d405e9c507053c06558e8fd6216aac874b8e7
Ivan Ratushnjak <ratushnjak.ivanko@gmail.com>
2026-05-09 18:31:58 +0300

    add task solution

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,14 @@
-import './MoviesList.scss';
+import React from 'react';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies }) => {
+  return (
+    <div className="container">
+      <ul className="movies-list" data-cy="moviesList">
+        {movies.map(movie => (
+          <MovieCard key={movie.imdbId} movie={movie} />
+        ))}
+      </ul>
+    </div>
+  );
+};
