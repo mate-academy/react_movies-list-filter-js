cec77c0808959f1b02d05c5ff161df6cfc4a5c63
Vladyslav Stepanenko <vlad.step.186@gmail.com>
2026-05-15 18:03:12 +0300

    fixed duplicates

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,11 @@ import moviesFromServer from './api/movies.json';
 
 export const App = () => {
   const [query, setQuery] = useState('');
+  const normalizedQuery = query.trim().toLowerCase();
   const visibleMovies = moviesFromServer.filter(
     movie =>
-      movie.title.toLowerCase().includes(query.trim().toLowerCase()) ||
-      movie.description.toLowerCase().includes(query.trim().toLowerCase()),
+      movie.title.toLowerCase().includes(normalizedQuery) ||
+      movie.description.toLowerCase().includes(normalizedQuery),
   );
   const handleQueryChange = event => {
     setQuery(event.target.value);
