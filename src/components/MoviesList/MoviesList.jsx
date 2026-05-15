2aa9bf589b7ad48a84238ed703b6a30b56061f2c
Natalia Lunkevych <gadyka08@gmail.com>
2026-05-15 18:20:03 +0300

    +

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,29 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
-  <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
-  </div>
-);
+export const MoviesList = ({ movies, query }) => {
+  // 1. Готуємо запит (прибираємо пробіли та регістр)
+  const normalizedQuery = query.trim().toLowerCase();
+
+  // 2. Створюємо масив тільки з тих фільмів, що нам підходять
+  const filteredMovies = movies.filter(movie => {
+    const titleMatch = movie.title.toLowerCase().includes(normalizedQuery);
+
+    // Тут нам треба додати таку ж перевірку для movie.description
+    const descriptionMatch = movie.description
+      .toLowerCase()
+      .includes(normalizedQuery);
+
+    // Повертаємо true, якщо знайдено хоча б в одному полі
+    return titleMatch || descriptionMatch;
+  });
+
+  // 3. Повертаємо JSX
+  return (
+    <div className="movies">
+      {filteredMovies.map(movie => (
+        <MovieCard key={movie.imdbId} movie={movie} />
+      ))}
+    </div>
+  );
+};
