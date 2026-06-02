189eec4fac9d8cca9ca005f77b9644aa56d47263
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-06-02 14:03:56 +0300

    Final Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,52 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
-import { useState } from 'react';
 
 function prepareMovies(goods, query) {
   let sorted = [...goods];
-  let newQuery = query.toLowerCase();
+  const newQuery = query.trim().toLowerCase();
 
-  sorted = sorted.filter(good => (
-    good.title.toLowerCase().includes(newQuery))
-    || (good.description.toLowerCase().includes(newQuery)));
+  sorted = sorted.filter(
+    good =>
+      good.title.toLowerCase().includes(newQuery) ||
+      good.description.toLowerCase().includes(newQuery),
+  );
 
   return sorted;
-};
+}
 
 export const App = () => {
-  const [sortField,setSortField] = useState('');
+  const [sortField, setSortField] = useState('');
   const prepare = prepareMovies(moviesFromServer, sortField);
 
-  return(
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
+  return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
 
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-              value={sortField}
-              onChange={(event) => (setSortField(event.target.value))}
-            />
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={sortField}
+                onChange={event => setSortField(event.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={prepare} />
       </div>
 
-      <MoviesList movies={prepare} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-);
-}
+  );
+};
