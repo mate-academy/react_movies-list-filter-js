7f13a25d813bae6aec0093e27754bba8a71e7478
Tetiana Topchii <rohatetiana@gmail.com>
2026-06-30 19:54:00 +0200

    solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.scss';
+import { useState } from 'react';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
-import { useState } from 'react';
 
 export const App = () => {
   const [query, setQuery] = useState('');
@@ -12,39 +12,38 @@ export const App = () => {
     const title = movie.title.toLowerCase();
     const description = movie.description?.toLowerCase() || '';
 
-    return title.includes(normalizedQuery)
-      || description.includes(normalizedQuery);
+    return (
+      title.includes(normalizedQuery) || description.includes(normalizedQuery)
+    );
   });
+
   return (
+    <div className="page">
+      <div className="page-content">
+        <div className="box">
+          <div className="field">
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label htmlFor="search-query" className="label">
+              Search movie
+            </label>
 
-  <div className="page">
-    <div className="page-content">
-      <div className="box">
-        <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-          <label htmlFor="search-query" className="label">
-            Search movie
-          </label>
-
-          <div className="control">
-            <input
-              type="text"
-              id="search-query"
-              className="input"
-              placeholder="Type search word"
-              value={query}
-              onChange={(e)=>{
-                setQuery(e.target.value)
-              }}
-            />
+            <div className="control">
+              <input
+                type="text"
+                id="search-query"
+                className="input"
+                placeholder="Type search word"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+              />
+            </div>
           </div>
         </div>
+
+        <MoviesList movies={visibleMovies} />
       </div>
 
-      <MoviesList movies={visibleMovies} />
+      <div className="sidebar">Sidebar goes here</div>
     </div>
-
-    <div className="sidebar">Sidebar goes here</div>
-  </div>
-  )
+  );
 };
