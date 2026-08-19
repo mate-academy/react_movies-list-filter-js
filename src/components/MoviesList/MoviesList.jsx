b5ffe4cc51e705ef072610c9041ce7b599327809
Oleksandr <oleksandr.sviderskyi@nure.ua>
2026-08-19 15:06:21 +0300

    task complete

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,20 @@
 import './MoviesList.scss';
 import { MovieCard } from '../MovieCard';
 
-export const MoviesList = ({ movies }) => (
+export const MoviesList = ({ movies, query }) => (
   <div className="movies">
-    {movies.map(movie => (
-      <MovieCard key={movie.imdbId} movie={movie} />
-    ))}
+    {movies.map(movie => {
+      let { title, description } = movie;
+      const myQuery = query.replaceAll(' ', '').toLowerCase();
+
+      title = title.replaceAll(' ', '').toLowerCase();
+      description = description.replaceAll(' ', '').toLowerCase();
+
+      if (title.includes(myQuery) || description.includes(myQuery)) {
+        return <MovieCard key={movie.imdbId} movie={movie} />;
+      }
+
+      return null;
+    })}
   </div>
 );
